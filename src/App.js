c9152f519017bc27af34117d7c810059029c9cd5
coldstoic <coldstoic@gmail.com>
2019-03-17 19:33:06 -0400

    cleaned up initial files

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+/* THIS IS THE MAIN TEMPLATE */
+
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
@@ -6,20 +8,9 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <img src={logo} className="App-logo" alt="logo"/>
+
+        <h1>UNDER DEVELOPMENT</h1>
       </div>
     );
   }
