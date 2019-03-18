b1b0597cbe88503dbc5ed81febbf7d49ba535914
coldstoic <coldstoic@gmail.com>
2019-03-18 14:24:08 -0400

    further cleanup

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 /* THIS IS THE MAIN TEMPLATE */
 
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <img src={logo} className="App-logo" alt="logo"/>
-
         <h1>UNDER DEVELOPMENT</h1>
       </div>
     );
