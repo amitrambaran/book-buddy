f626dd44e823019201887018859aaedbf6254074
coldstoic <coldstoic@gmail.com>
2019-03-18 14:51:51 -0400

    bookshelf adjustments

--- src/components/bookshelf/bookItem.js
+++ src/components/bookshelf/bookItem.js
@@ -5,7 +5,7 @@ export class bookItem extends Component {
     return (
       <div>
         <h2>{ this.props.book.title }</h2>
-        <img src={ this.props.book.cover }></img>
+        <img src={ this.props.book.cover } alt={ this.props.book.title + ' cover art'} width="150" height="240"></img>
         <h3>{ this.props.book.author }</h3>
         <h3>{ this.props.book.rating }</h3>
 
