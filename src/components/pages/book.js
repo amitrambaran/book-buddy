9c002160bda1e629b0a0d6538b0ec121870a8629
coldstoic <coldstoic@gmail.com>
2019-03-18 19:02:01 -0400

    mapping api call and data, added more dummy data

--- src/components/pages/book.js
+++ src/components/pages/book.js
@@ -3,17 +3,20 @@ import Axios from 'axios';
 
 export default class book extends Component {
   state = {
-    id: ''
+    id: '',
+    book: []
   }
 
   // Triggers on component mount.
   componentDidMount() {
     var id = window.location.pathname.split('/')[2]
     this.setState({ id: id })
-    
+
     // Get API information here...
-    Axios.get('https://jsonplaceholder.typicode.com/todos').then(res =>
-      console.log(res.data)
+    var URL = 'http://my-json-server.typicode.com/amitrambaran/book-buddy/books/' + id
+    console.log(URL)
+    Axios.get(URL).then(res =>
+      this.setState({ book: res.data })
     )
   }
 
@@ -24,6 +27,10 @@ export default class book extends Component {
         <p>Page shows a given book's title, author, rating, desc, where to buy, etc..</p>
         <br/>
         <p>Book ID: { this.state.id }</p>
+        <p>Title: { this.state.book.title }</p>
+        <p>Author: { this.state.book.author }</p>
+        <p>Rating: { this.state.book.rating }</p>
+        <p>Description: { this.state.book.description }</p>
     </React.Fragment>
     )
   }
