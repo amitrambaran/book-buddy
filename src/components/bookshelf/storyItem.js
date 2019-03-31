fc9a1107c06b4222b7f6dced188cf018b5b00ca4
coldstoic <coldstoic@gmail.com>
2019-03-31 17:23:23 -0400

    fixed responsive scaling

--- src/components/bookshelf/storyItem.js
+++ src/components/bookshelf/storyItem.js
@@ -10,23 +10,27 @@ export class storyItem extends Component {
   render() {
     return (
       // Direct to book.js page upon clicking the item. Appends book ID to the URL.
-      <Link
-        to={ "/story/" + this.props.story.ID }
-        onClick={ this.forceUpdate }
-        style={{ textDecoration: 'none', color: 'black' }}
-      >
-        <Card style={{ maxWidth: '250px' }} className="book-panel">
-          <Card.Header style={{ fontSize: '100px', color: 'white', backgroundColor: '#343a40'}}>{ this.props.story.title.substring(0, 1) }</Card.Header>
-          <Card.Body>
-            <Card.Title>{ this.props.story.title }</Card.Title>
-            <Card.Text style={{ fontSize: '14px', margin: '0px'}}>
-              Author: { this.props.story.author }
-              <br/><br/>
-              { this.props.story.content.substring(0, 200) }
-            </Card.Text>
-          </Card.Body>
-        </Card>
-      </Link>
+      <div className="book-panel">
+        <div>
+          <Link
+            to={ "/story/" + this.props.story.ID }
+            onClick={ this.forceUpdate }
+            style={{ textDecoration: 'none', color: 'black'}}
+          >
+            <Card>
+              <Card.Header style={{ fontSize: '100px', color: 'white', backgroundColor: '#343a40'}}>{ this.props.story.title.substring(0, 1) }</Card.Header>
+              <Card.Body>
+                <Card.Title>{ this.props.story.title }</Card.Title>
+                <Card.Text style={{ fontSize: '14px', margin: '0px'}}>
+                  Author: { this.props.story.author }
+                  <br/><br/>
+                  { this.props.story.content.substring(0, 200) }
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Link>
+        </div>
+      </div>
     )
   }
 }
