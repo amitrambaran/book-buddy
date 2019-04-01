2e8aea9fe99a4584015a03ea0f008eb555410780
Lionel Pereira <lionel.m.pereira@gmail.com>
2019-04-01 01:15:41 -0400

    Removed uneccessary files

--- src/components/pages/about.js
+++ src/components/pages/about.js
@@ -5,8 +5,24 @@ export default class about extends Component {
     return (
       <React.Fragment>
       <h1>About</h1>
+      <ul style={{listStyle: 'none'}}>
+      This app was powered by:
+        <li>
+          <a href="https://openlibrary.org/developers/api"> OpenLibrary API</a>
+        </li>
+        <li>
+          <a href="https://tastedive.com/read/api">TasteDive Recommendation API</a>
+        </li>
+      </ul>
       <h2>Team Members</h2>
-      <p>...</p>
+      <ul style={{listStyle: 'none'}}>
+        <li>Peter Nguyen</li>
+        <li>Lionel Pereira</li>
+        <li>Chloe Ierullo</li>
+        <li style={{ fontSize: '0.1em'}}>Amit Rambaran</li>
+        <li style={{ fontSize: '0.1em'}}>Mohamad Al-Jaf</li>
+        <li style={{ fontSize: '0.1em'}}>Sai Nidumukkala</li>
+      </ul>
     </React.Fragment>
     )
   }
